fb7aadbcc2f1f5fafe463ce1d5417ea2e4580ccc
Kelsey Geiger <kielgeiger@gmail.com>
2016-12-08 10:17:56 -0800

    Minor updates, attempt at 3D.

--- lidar_visual.py
+++ lidar_visual.py
@@ -38,7 +38,9 @@ MAX_DIST = 6000.0
 
 RAW = 0
 POLAR = 1
-THREE_DIM = 2
+EUCLID_3D = 2
+PRETTY_POLAR = 3
+PRETTY_3D = 4
 
 class LidarVisualizer:
     
@@ -46,13 +48,14 @@ class LidarVisualizer:
         
         self.lidar = lidar
         
-        self.projection = perspective(-MAX_DIST, MAX_DIST, win_h/2, -win_h/2, 5/MAX_DIST, 1.0)
+        self.projection = perspective(-MAX_DIST, MAX_DIST, win_h/2, -win_h/2, 1, MAX_DIST)
         self.viewport = view(0, 0, win_w, win_h)
         
         self.height = win_h
         self.width = win_w
         self.raw_data = []
         self.points = []
+        self.depth_buf = [[MAX_DIST-1] * self.width] * self.height
 
         self.running = True
         self.mode = POLAR
@@ -63,7 +66,6 @@ class LidarVisualizer:
 
     def update_data(self, distances):
         self.raw_data = distances
-    
     
     def graph_dists(self, surface, dists, colors):
         white = sdl2.ext.Color(255, 255, 255)
@@ -127,7 +129,7 @@ class LidarVisualizer:
             
             #transform to screen space
             point = (np.array([x, z, 0, 1]) * self.viewport).A1
-            point = [int(point[0]), self.height - int(point[1])]
+            point = [int(point[0] / point[3]), self.height - int(point[1] / point[3])]
             
             coords.append(point)
         
@@ -145,28 +147,83 @@ class LidarVisualizer:
         
     
     def draw_3D(self):
-        pass
+        dists = [d[0] for d in self.raw_data]
+        quals = [q[0] for q in self.raw_data]
+
+        max_qual = max(quals)
+
+        coords = []
+        colors = []
         
+        for i in range(len(self.raw_data)):
+            c = -math.cos(math.pi * i / 180)
+            s = -math.sin(math.pi * i / 180)
+            
+            x = c * dists[i]
+            z = s * dists[i]
+            
+            r = float(max_qual - quals[i]) / max_qual
+            g = float(quals[i])/max_qual
+            b = 1.0
+            
+            gray = 1.0 - (dists[i] / MAX_DIST)
+            r = r * gray
+            g = g * gray
+            b = gray
+            
+            color = sdl2.ext.Color(int(255 * r), int(255 * g), int(255 * b))
+            colors.append(color)
+            
+            floor = (np.array([x, -self.height/2.0, z, 1]) * self.projection * self.viewport).A1
+            ceil = (np.array([x, self.height/2.0, z, 1]) * self.projection * self.viewport).A1
+            
+            floor = np.array([int(floor[0]), int(floor[1]), int(floor[2])])
+            ceil = np.array([int(ceil[0]), int(ceil[1]), int(ceil[2])])
+            
+            coords.append((floor, ceil))
+        
+        win_surf = self.window.get_surface()
+        
+        white = sdl2.ext.Color(255, 255, 255)
+        sdl2.ext.fill(win_surf, white)
+        
+        pix_view = sdl2.ext.pixels2d(win_surf)
+        for i in range(len(coords)):
+            x = coords[i][0][0]
+            y = coords[i][0][1]
+            z = coords[i][0][2]
+            
+            while y < coords[i][1][1]:
+                if self.depth_buf[y][x] > z:
+                    self.depth_buf[y][x] = z
+                    
+                    color = colors[i]
+                    pix_view[y][x] = color
+                    y = y + 1
+                
+    def refresh(self):
+        self.update_data(self.lidar.get_image())
+        while sdl2.SDL_PollEvent(ctypes.byref(event)) != 0:
+            if event.type == sdl2.SDL_QUIT:
+                self.running = False
+                break
+                
+        print("Drawing...")
+        if self.mode == RAW:
+            self.draw_raw()
+        elif self.mode == POLAR:
+            self.draw_polar()
+        elif self.mode == EUCLID_3D:
+            self.draw_3D()
+             
+        print("Refreshing...")
+        self.window.refresh()
+    
     def run(self):
         
         event = sdl2.SDL_Event()
         while self.running:
-            #time.sleep(0.00001)
-            self.update_data(self.lidar.get_image())
-            while sdl2.SDL_PollEvent(ctypes.byref(event)) != 0:
-                if event.type == sdl2.SDL_QUIT:
-                    self.running = False
-                    break
-                
-                
-            print("Drawing...")
-            if self.mode == RAW:
-                self.draw_raw()
-            elif self.mode == POLAR:
-                self.draw_polar()
             
-            print("Refreshing...")
-            self.window.refresh()
         
         sdl2.SDL_Quit()
         return 0
